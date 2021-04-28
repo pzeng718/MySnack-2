5bae77e82b09854d18aab2730787edf8dbce0a54
lejoey95 <77023048+lejoey95@users.noreply.github.com>
2021-04-28 15:55:04 -0700

    Product-info.js page is done

--- WebContent/product-info.js
+++ WebContent/product-info.js
@@ -17,39 +17,37 @@ function getParameterByName(target) {
 function handleResult(resultDataString) {
     console.log("Hello");
     console.log(resultDataString);
+    let item_name = resultDataString.name;
+    let item_price = resultDataString.price;
+    let item_description = resultDataString.description;
+    let item_quantity = resultDataString.qty;
+    let item_mnf = resultDataString.manufacturer;
+    let item_imgs = resultDataString.images.split(",");
+
+
+    let productNameBody = $("#productName");
+    let infoBody = $(".info");
+    let orderBtnBody = $(".order-btn");
+
+
+    productNameBody.append(`<i>${item_name}</i>`);
+
+
+    infoBody.append(`<img src=${item_imgs[0]}>`);
+    infoBody.append(`<img src=${item_imgs[1]}>`);
+
+    infoBody.append(
+        `<p>Quantity left available: <strong>${item_quantity}</strong></p>`
+    );
+    infoBody.append(`<p>Price: <strong>${item_price}</strong></p>`);
+    infoBody.append(
+        `<p>Manufactured by: <strong>${item_mnf}</strong></p>`
+    );
+    infoBody.append(`<p>- <i>${item_description}</i></p>`);
+
+    orderBtnBody.append(`<button type="submit">Order this product</button>`);
 }
-//
-//     $("#productName").val(resultDataString[name]);
 
-//let id = resultDataString[id];
-/*let url = new URL(window.location.href);
-let product_id = parseInt(url.searchParams.get("product_id"));
-
-let product = products.find((p) => p.id === product_id);
-
-let productNameBody = jQuery("#productName");
-let orderBtnBody = jQuery(".order-btn");
-let infoBody = jQuery(".info");
-
-productNameBody.append(`<i>${product.name}</i>`);
-
-for (let imageSrc of product.image) {
-  infoBody.append(`<img src=${imageSrc}>`);
-}
-
-infoBody.append(
-    `<p>Quantity left available: <strong>${product.qty}</strong></p>`
-);
-infoBody.append(`<p>Price: <strong>${product.price}</strong></p>`);
-infoBody.append(
-    `<p>Manufactured by: <strong>${product.manufacturer}</strong></p>`
-);
-infoBody.append(`<p>- <i>${product.description}</i></p>`);
-
-orderBtnBody.append(`<form action="order-page.html?product_id=${product_id}" method="POST">
-<button type="submit">Order this product</button>
-</form>`);*/
-//}
 
 let productId = getParameterByName('product_id');
 
