be8e7506f38e4a46f4840d8cc6926ce15e1c5839
JOEY-PC\Joey <ngockl@uci.edu>
2021-05-02 22:19:39 -0700

    Add selected amount display on the order history and make the font become bold

--- WebContent/index.js
+++ WebContent/index.js
@@ -39,7 +39,7 @@ const row = `
     <div class="row">
         <div class="col-sm">:product_name</div>
         <div class="col-sm">:unit_price</div>
-<!--        <div class="col-sm">:select_amount</div>-->
+        <div class="col-sm">:select_amount</div>
 <!--        <div class="col-sm">:created_at</div>-->
 <!--        <div class="col-sm">:order_total_price</div>-->
 <!--        <div class="col-sm">:shipping_address</div>-->
@@ -54,7 +54,7 @@ function handleRecentlyBoughtItemsData(resultDataString) {
     let newRow = row;
     newRow = newRow.replace(':product_name', product.name);
     newRow = newRow.replace(':unit_price', product.price);
-    // newRow = newRow.replace(':select_amount', product.selected_amount);
+    newRow = newRow.replace(':select_amount', product.selected_amount);
     // newRow = newRow.replace(':created_at', product.created_at);
     // newRow = newRow.replace(':order_total_price', product.order_total_price);
     // newRow = newRow.replace(':shipping_address', product.order_shipping_address);
